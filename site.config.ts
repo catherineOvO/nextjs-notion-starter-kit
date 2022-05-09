3a722bd1331729fe3108dd74f2b4b5ac9558264d
catherineOvO <kikumuru@gmail.com>
2022-05-08 23:37:46 -0400

    add publications

--- site.config.ts
+++ site.config.ts
@@ -48,16 +48,16 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  // navigationStyle: 'default'
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'Publications',
+      pageId: 'a6df271d0259455f8cd221546da62411'
+    },
+    // {
+    //   title: 'CV',
+    //   pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
+    // }
+  ]
 })
